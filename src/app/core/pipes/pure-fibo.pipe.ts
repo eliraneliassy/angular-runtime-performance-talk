fa9ffe339eb3508535fd244a4de0617c281d4e5a
Eliran Eliassy <eliran@stuccomedia.com>
2018-06-28 15:59:05 +0300

    add memo decorator

--- src/app/core/pipes/pure-fibo.pipe.ts
+++ src/app/core/pipes/pure-fibo.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { fibonacci } from '../utils/fibonacci';
 
+import memo from 'memo-decorator';
+
 @Pipe({
   name: 'pureFibo',
   pure: true
 })
 export class PureFiboPipe implements PipeTransform {
-
+  @memo()
   transform(value: any, args?: any): any {
     console.log('asdasd');
     return fibonacci(value);
